22df34e67e725046c7ff92238a9c772634be6855
Vivi <tooproforspam@gmail.com>
2024-11-19 20:20:34 -0600

    Add project files.

---
+++ DalamudKey Updater/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DalamudKey_Updater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.SaveBranchButton = new System.Windows.Forms.Button();
+            this.requesttimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.BranchComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // SaveBranchButton
+            // 
+            this.SaveBranchButton.Location = new System.Drawing.Point(173, 28);
+            this.SaveBranchButton.Name = "SaveBranchButton";
+            this.SaveBranchButton.Size = new System.Drawing.Size(184, 41);
+            this.SaveBranchButton.TabIndex = 0;
+            this.SaveBranchButton.Text = "Set Branch";
+            this.SaveBranchButton.UseVisualStyleBackColor = true;
+            this.SaveBranchButton.Click += new System.EventHandler(this.SaveBranchButton_Click);
+            // 
+            // requesttimer
+            // 
+            this.requesttimer.Enabled = true;
+            this.requesttimer.Tick += new System.EventHandler(this.requesttimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(136, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Loading branches";
+            // 
+            // BranchComboBox
+            // 
+            this.BranchComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.BranchComboBox.FormattingEnabled = true;
+            this.BranchComboBox.Location = new System.Drawing.Point(16, 35);
+            this.BranchComboBox.Name = "BranchComboBox";
+            this.BranchComboBox.Size = new System.Drawing.Size(151, 28);
+            this.BranchComboBox.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(369, 81);
+            this.Controls.Add(this.BranchComboBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SaveBranchButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Dalamud Key Updater";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SaveBranchButton;
+        private System.Windows.Forms.Timer requesttimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox BranchComboBox;
+    }
+}
+
---
+++ DalamudKey Updater/Form1.cs
@@ -0,0 +1,140 @@
+﻿using DalamudKey_Updater.Properties;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DalamudKey_Updater
+{
+    public partial class Form1 : Form
+    {
+
+        private const string BranchInfoUrl = "https://kamori.goats.dev/Dalamud/Release/Meta";
+        private Dictionary<string, VersionEntry> branches;
+        private string rawjson;
+        private bool ready = false;
+        private List<VersionEntry> versions = new List<VersionEntry>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void requesttimer_Tick(object sender, EventArgs e)
+        {
+            if (!ready)
+            {
+                label1.Text = "Loading branches...";
+                return;
+            }
+
+            BranchComboBox.Items.AddRange(versions.ToArray());
+            BranchComboBox.SelectedIndex = 0;
+
+            requesttimer.Enabled = false;
+
+            label1.Text = "Select a branch";
+            //MessageBox.Show(rawjson);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Task.Run(async () =>
+            {
+                using (WebClient wc = new WebClient())
+                {
+                    rawjson = wc.DownloadString(BranchInfoUrl);
+                    Console.WriteLine(rawjson);
+
+                    branches = JsonConvert.DeserializeObject<Dictionary<string, VersionEntry>>(rawjson);
+                    Console.WriteLine(branches);
+
+
+                    foreach(var item in branches)
+                    {
+                        versions.Add(item.Value);
+                    }
+
+                    Console.WriteLine($"{versions.Count} versions to select");
+
+                    ready = true;
+                }
+            });
+        }
+
+
+        private class VersionEntry
+        {
+            [JsonProperty("key")]
+            public string Key { get; set; }
+
+            [JsonProperty("track")]
+            public string Track { get; set; }
+
+            [JsonProperty("assemblyVersion")]
+            public string AssemblyVersion { get; set; }
+
+            [JsonProperty("runtimeVersion")]
+            public string RuntimeVersion { get; set; }
+
+            [JsonProperty("runtimeRequired")]
+            public bool RuntimeRequired { get; set; }
+
+            [JsonProperty("supportedGameVer")]
+            public string SupportedGameVer { get; set; }
+
+            [JsonProperty("downloadUrl")]
+            public string DownloadUrl { get; set; }
+
+            [JsonProperty("gitSha")]
+            public string GitSha { get; set; }
+
+            public override string ToString()
+            {
+                return Track.ToString();
+            }
+        }
+
+        private void SaveBranchButton_Click(object sender, EventArgs e)
+        {
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string filePath = Path.Combine(appDataPath, "XIVLauncher", "DalamudConfig.json");
+            VersionEntry selected = BranchComboBox.SelectedItem as VersionEntry;
+
+            try
+            {
+                // Read the JSON file content
+                string jsonContent = File.ReadAllText(filePath);
+
+                // Parse the JSON content into a JObject
+                JObject jsonObject = JObject.Parse(jsonContent);
+
+                // Edit the keys
+                jsonObject["DalamudBetaKey"] = selected.Key;
+                jsonObject["DalamudBetaKind"] = selected.Track;
+
+                // Serialize the updated JObject back to a JSON string
+                string updatedJsonContent = JsonConvert.SerializeObject(jsonObject, Formatting.Indented);
+
+                // Write the updated JSON back to the file
+                File.WriteAllText(filePath, updatedJsonContent);
+
+                Console.WriteLine("JSON file updated successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
